8fd7c8a039d2c4100aca7482713dd9c8e83abe29
nizam <nizamstudymajliz@gmail.com>
2021-09-10 22:59:43 +0530

    calculator-with time

--- src/Componants/Time.js
+++ src/Componants/Time.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
+import "./Time.css"
 
 const Time = () =>{
    let time = new Date().toLocaleTimeString();
 
    const [ctime, setCtime] = useState(time);
    
+   const Utime =()=>{
+       time = new Date().toLocaleTimeString();
+       setCtime(time);
+   };
+
+   setInterval(Utime, 1000)
+
     return (
-        <div>
+        <div className="time">
             <h1>{ctime}</h1>
         </div>
     );
